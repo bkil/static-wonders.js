31ee142d948705898b36ad0f9c905f0a3d6e251c
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-06-28 20:08:01 +0200

    userjs/jofogas.hu: support disabling storage (cookies) in the browser

--- userjs/jofogas.hu.user.js
+++ userjs/jofogas.hu.user.js
@@ -5,17 +5,30 @@
 // @namespace   bkil.hu
 // @match       https://*.jofogas.hu/*
 // @grant       none
-// @version     2022.6.1
+// @version     2022.6.26
 // @license     MIT
-// @homepageURL https://gitlab.com/bkil/static-wonders.js
 // @homepageURL https://github.com/bkil/static-wonders.js
 // @supportURL  https://gitlab.com/bkil/static-wonders.js/issues
 // @noframes
 // ==/UserScript==
 
-if (!(typeof window.Didomi === 'object' && window.Didomi !== null)) {
+(function() {
+'use strict';
+
+  if (!(typeof window.Didomi === 'object' && window.Didomi !== null)) {
     window.Didomi = new Object();
     window.Didomi.preferences = new Object();
     window.Didomi.preferences.show = function() {};
     window.Didomi.getUserConsentStatusForVendor = function(x) { return false; };
-}
+  }
+
+  /* if you disable localStorage (cookies) in the browser, window.sessionStorage will throw otherwise */
+  angular.module('jofogas').factory('sessionStorageService', [function() {
+    let e = {};
+    e.set = e.get = e.remove = e.register = e.getRegisteredKeys = e.removeRegisteredKeys = e.clearRegister = e.cleanUp = function (v) {
+      return null;
+    }
+    return e;
+  }]);
+
+})();
