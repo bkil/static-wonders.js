22146372e55dd93de9f325e94328135294cc8f4b
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-12-12 21:18:35 +0100

    gigablast.com.user.js: support filter options

--- userjs/gigablast.com.user.js
+++ userjs/gigablast.com.user.js
@@ -5,7 +5,7 @@
 // @namespace   bkil.hu
 // @match       https://gigablast.com/*
 // @grant       none
-// @version     2022.12.1
+// @version     2022.12.2
 // @license     MIT
 // @homepageURL https://github.com/bkil/static-wonders.js
 // @supportURL  https://gitlab.com/bkil/static-wonders.js/issues
@@ -14,6 +14,20 @@
 
 (function() {
 'use strict';
+
+  function init(style) {
+    const s = document.createElement('style');
+    s.textContent = style;
+    document.head.appendChild(s);
+    const r =
+      document.body.getAttribute('onload')
+      ?.match(/(&rand=[^']*)'([^']|\n)*'([^']*)'/);
+    if (r) {
+      get(window.location + r[1] + r[3]);
+    } else {
+      document.getElementById('bodycont').innerText = 'UserJS failed to parse nonce from HTML';
+    }
+  }
 
   function get(url) {
     const xhr = new XMLHttpRequest();
@@ -27,12 +41,30 @@
     xhr.send();
   }
 
-  const r =
-    document.body.getAttribute('onload')
-    .match(/(&rand=[^']*)'([^']|\n)*'([^']*)'/);
-  if (r) {
-    get(window.location + r[1] + r[3]);
-  } else {
-    document.getElementById('bodycont').innerText = 'UserJS failed to parse nonce from HTML';
-  }
+init(`
+    #filters {
+        line-height: initial !important;
+        max-width: initial !important;
+    }
+
+    #filters #hiddenfilters {
+        display: inline-block !important;
+    }
+
+    #filters #hiddenfilters > nobr {
+        display: none;
+    }
+
+    #filters #hiddenfilters span[onmousedown] {
+        display: inline-block !important;
+        position: initial !important;
+        max-height: 7em !important;
+        font-size: xx-small;
+        margin: 0 !important;
+    }
+
+    #filters #hiddenfilters div {
+        padding: 0 !important;
+    }
+  `);
 })();
