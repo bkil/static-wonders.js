db752a3608cb5a81cbe4b77ada3a23bbc242e797
bkil <b.K.il.h.u+tigbuh@gmail.com>
2024-01-01 15:10:35 +0100

    userjs/ietf.template: misskey.io, wiki.js.org

--- userjs/wiki.js.user.js
+++ userjs/wiki.js.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name        wiki.ietf.org noJS
+// @name        wiki.js.org ietf misskey noJS
 // @author      bkil
 // @description Minimal viewer of template
 // @namespace   bkil.hu
 // @match       https://wiki.ietf.org/*
+// @match       https://wiki.misskey.io/*
 // @grant       none
-// @version     2023.7.1
+// @version     2023.12.1
 // @license     MIT
 // @homepageURL https://gitlab.com/bkil/static-wonders.js
 // @homepageURL https://github.com/bkil/static-wonders.js
